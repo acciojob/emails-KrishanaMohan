d1212ad9ea73ebfa43cf636651ab3ea939aa55df
KrishanaMohan <ram227209@gmail.com>
2022-12-24 15:44:09 +0530

    ok

--- src/main/java/com/driver/Meeting.java
+++ src/main/java/com/driver/Meeting.java
@@ -5,7 +5,6 @@ import java.time.LocalTime;
 public class Meeting {
     private LocalTime startTime;
     private LocalTime endTime;
-
     public Meeting(LocalTime startTime, LocalTime endTime) {
         this.startTime = startTime;
         this.endTime = endTime;
