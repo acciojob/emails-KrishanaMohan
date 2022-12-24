1b4ea99f5b816247e9734ca47a8764b980f23574
KrishanaMohan <ram227209@gmail.com>
2022-12-24 17:20:07 +0530

    ok

--- src/main/java/com/driver/Email.java
+++ src/main/java/com/driver/Email.java
@@ -1,5 +1,8 @@
 package com.driver;
 
+import java.util.Arrays;
+import java.util.HashSet;
+
 public class Email {
 
     private String emailId;
@@ -19,34 +22,43 @@ public class Email {
     }
 
     public void changePassword(String oldPassword, String newPassword){
-        if(password.equals(oldPassword)){
-           boolean size=false;
-           boolean upper=false;
-           boolean lower=false;
-           boolean digit=false;
-           boolean special=false;
-           for(int i=0;i<newPassword.length();i++){
-               char c=newPassword.charAt(i);
-               if(newPassword.length()>=8)
-                   size=true;
-               if(Character.isUpperCase(c))
-                   upper=true;
-               if(Character.isLowerCase(c))
-                   lower=true;
-                if(Character.isDigit(c))
-                   digit=true;
-                if(Character.isSpaceChar(c))
-                    special=true;
-           }
-           if(size && upper && lower && digit && special){
-               password=newPassword;
-           }
-        }
         //Change password only if the oldPassword is equal to current password and the new password meets all of the following:
         // 1. It contains at least 8 characters
         // 2. It contains at least one uppercase letter
         // 3. It contains at least one lowercase letter
         // 4. It contains at least one digit
         // 5. It contains at least one special character. Any character apart from alphabets and digits is a special character
+        if(oldPassword.equals(password)){
+            int size = newPassword.length();
+            boolean hasSize = false;
+            boolean hasUpper = false;
+            boolean hasLower = false;
+            boolean hasDigit = false;
+            boolean specialChar = false;
+
+            if(size >= 8)
+                hasSize = true;
+
+            HashSet<Character> set = new HashSet<>(Arrays.asList('!','@','#','$','%','^','&','*','(',')','-',',','+'));
+
+            for(char i : newPassword.toCharArray()){
+                if(Character.isUpperCase(i))
+                    hasUpper = true;
+
+                if(Character.isLowerCase(i))
+                    hasLower = true;
+
+                if(Character.isDigit(i))
+                    hasDigit = true;
+
+                if(set.contains(i))
+                    specialChar = true;
+
+            }
+
+            if(hasSize && hasUpper && hasLower && hasDigit && specialChar){
+                this.password = newPassword;
+            }
+        }
     }
 }
